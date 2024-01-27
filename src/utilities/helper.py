b0079ac6f893edca535d444e2a960a4f06fcb290
ryoiwata <ryoiwata@github.com>
2024-01-27 17:09:36 -0500

    refactor: renaming closest to nearest

--- src/utilities/helper.py
+++ src/utilities/helper.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 """
 """
+from collections import defaultdict
 
+def create_recursive_dict():
+    """
+    Creates a recursive dictionary using defaultdict.
+
+    This function returns a defaultdict that defaults to itself. This allows for the creation of 
+    dictionaries of arbitrary depth without having to manually check if a key exists before adding 
+    to it.
+
+    Returns:
+    - defaultdict: A defaultdict that defaults to itself, allowing for the creation of dictionaries 
+                   of arbitrary depth.
+    """
+    return defaultdict(create_recursive_dict)
 
 def compute_sorted_index(group, value_column='Value', index_column='SortedIndex'):
     """ 
@@ -20,25 +34,25 @@ def compute_sorted_index(group, value_column='Value', index_column='SortedIndex'
     return group
 
 
-def find_closest_index(sorted_list=None, target=0):
+def find_nearest_index(sorted_list=None, target=0):
     """
-    Returns the index of the number in the sorted list that is closest to the target.
+    Returns the index of the number in the sorted list that is nearest to the target.
 
-    This function performs a binary search on a sorted list to find the closest number to 
+    This function performs a binary search on a sorted list to find the nearest number to 
     a given target. If the target exists in the list, its index is returned. If not, the 
-    function will return the index of the number that's closest to the target.
+    function will return the index of the number that's nearest to the target.
 
     Parameters:
     - sorted_list (list[int or float]): A sorted list of numbers.
-    - target (int or float): The target number to find the closest value to.
+    - target (int or float): The target number to find the nearest value to.
 
     Returns:
-    - int: The index of the closest number in the sorted list to the target. 
+    - int: The index of the nearest number in the sorted list to the target. 
            If the sorted list is empty, returns None.
 
     Example:
     >>> sorted_nums = [1, 3, 5, 8, 10, 15, 18, 20, 24, 27, 30]
-    >>> find_closest_index(sorted_nums, 6)
+    >>> find_nearest_index(sorted_nums, 6)
     2
 
     Note:
@@ -71,6 +85,24 @@ def find_closest_index(sorted_list=None, target=0):
     else:
         return right
 
+def find_nearest_indices(array1, array2):
+    """
+    Finds the indices of the elements in array2 that are nearest to the elements in array1.
+
+    This function flattens array1 and for each number in the flattened array, finds the index of the 
+    number in array2 that is nearest to it. The indices are then reshaped to match the shape of array1.
+
+    Parameters:
+    - array1 (numpy.ndarray): The array to find the nearest numbers to.
+    - array2 (numpy.ndarray): The array to find the nearest numbers in.
+
+    Returns:
+    - numpy.ndarray: An array of the same shape as array1, containing the indices of the nearest numbers 
+                     in array2 to the numbers in array1.
+    """
+    array1_flat = array1.flatten()
+    indices = np.array([np.abs(array2 - num).argmin() for num in array1_flat])
+    return indices.reshape(array1.shape)
 
 def main():
     """
